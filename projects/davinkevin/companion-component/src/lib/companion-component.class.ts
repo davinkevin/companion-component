695bb0db19c5c01e6df296fb048e33f1d35c8342
davinkevin <davin.kevin@gmail.com>
2018-05-27 13:49:47 +0200

    fix(): move to takeUntil of any instead of void 😅

--- projects/davinkevin/companion-component/src/lib/companion-component.class.ts
+++ projects/davinkevin/companion-component/src/lib/companion-component.class.ts
@@ -3,14 +3,14 @@ import {MonoTypeOperatorFunction, Subject} from 'rxjs';
 
 export class CompanionComponent {
   private readonly observable: Subject<void>;
-  private readonly takeUntil: MonoTypeOperatorFunction<void>;
+  private readonly takeUntil: MonoTypeOperatorFunction<any>;
 
   constructor() {
     this.observable = new Subject<void>();
     this.takeUntil = takeUntil(this.observable);
   }
 
-  untilDestroy(): () => MonoTypeOperatorFunction<void> {
+  untilDestroy(): () => MonoTypeOperatorFunction<any> {
     return () => this.takeUntil;
   }
 
